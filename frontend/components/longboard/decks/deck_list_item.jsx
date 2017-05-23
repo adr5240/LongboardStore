de0df897ba1c9635a2f3487abd2998d14435e685
Adam Reiter <adr5240@gmail.com>
2017-05-23 17:39:29 -0400

    Add higher res photos

--- frontend/components/longboard/decks/deck_list_item.jsx
+++ frontend/components/longboard/decks/deck_list_item.jsx
@@ -16,10 +16,10 @@ class DeckListItem extends React.Component {
     render() {
         const { deck } = this.props;
         return (
-            <div className="tile">
+            <div className="tile deckTile">
                 <ul>
                     <li className="nameTitle" key={1} onClick={ this._handleClick }>{deck.name}</li>
-                    <img key={`thumbnail`} onClick={ this._handleClick } src={deck.images[0].image_url} />
+                    <img className="deckList" key={`thumbnail`} onClick={ this._handleClick } src={deck.images[0].image_url} />
                     <ul>
                         <li key={2}><strong>Price:</strong> ${deck.price / 100}</li>
                         <li key={3}><strong>Length:</strong> {deck.length}"</li>
