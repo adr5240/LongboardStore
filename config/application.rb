05812c8fdeda0a8b106ef229e027a63ae3f5339e
Adam Reiter <adr5240@gmail.com>
2017-06-22 10:02:29 -0400

    Fix merge error

--- config/application.rb
+++ config/application.rb
@@ -23,9 +23,9 @@ module LongboardStoreApp
     config.paperclip_defaults = {
         storage: :s3,
         s3_credentials: {
-            bucket: ENV.fetch('BUCKET_NAME'),
-            access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-            secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+            bucket: ENV['s3_bucket'],
+            access_key_id: ENV['s3_access_key_id'],
+            secret_access_key: ENV['s3_secret_access_key'],
             s3_region: "us-east-1",
         }
     }
