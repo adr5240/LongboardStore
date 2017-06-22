738ba9a6f7a136d1ce24cf7e4507d60b0536498e
Adam Reiter <adr5240@gmail.com>
2017-06-22 11:31:55 -0400

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ var devPlugins = [];
 var prodPlugins = [
     new webpack.DefinePlugin({
         'process.env': {
-            'NODE_ENV': JSON.stringify('production')
+            NODE_ENV: JSON.stringify('production')
         }
     }),
     new webpack.optimize.UglifyJsPlugin({
