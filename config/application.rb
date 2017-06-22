a2875dfd0c8f99f729cfed8ef886dd547948bf09
Adam Reiter <adr5240@gmail.com>
2017-06-22 12:03:40 -0400

    Fix https error

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module LongboardStoreApp
             access_key_id: ENV['AWS_ACCESS_KEY_ID'],
             secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
             s3_region: "us-east-1",
+            s3_protocol: :https
         }
     }
 
